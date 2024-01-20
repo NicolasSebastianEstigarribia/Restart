4655eee0c0c25b92ef74529d8061b9d0d1cb919b
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-20 10:12:22 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Nicolas Estigarribia on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
