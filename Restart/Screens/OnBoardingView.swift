99fb3a58666eabe9e71edd1bc127ab9c295d5a2f
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-20 11:05:10 -0300

    Agregamos la funcionalidad basica

---
+++ Restart/Screens/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Restart
+//
+//  Created by Nicolas Estigarribia on 20/01/2024.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
