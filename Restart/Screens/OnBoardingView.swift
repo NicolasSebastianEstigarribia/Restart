8fc8fdcc5fe4ce9d7f5b1ec7daf6e5ecad5cb992
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-21 16:40:13 -0300

    Footer static sin funcionamiento de arrastras

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -61,7 +61,62 @@ struct OnBoardingView: View {
                 Spacer()
                 
                 // MARK: - FOOTER
-                Text("hi")
+                ZStack {
+                  // PARTS OF THE CUSTOM BUTTON
+                  
+                  // 1. BACKGROUND (STATIC)
+                  
+                  Capsule()
+                    .fill(Color.white.opacity(0.2))
+                  
+                  Capsule()
+                    .fill(Color.white.opacity(0.2))
+                    .padding(8)
+                  
+                  // 2. CALL-TO-ACTION (STATIC)
+                  
+                  Text("Empezar")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .offset(x:20)
+                  
+                  // 3. CAPSULE (DYNAMIC WIDTH)
+                  
+                  HStack {
+                    Capsule()
+                      .fill(Color("ColorRed"))
+                      .frame(width: 80)
+                    
+                    Spacer()
+                  }
+                  
+                  // 4. CIRCLE (DRAGGABLE)
+                  
+                  HStack {
+                    ZStack {
+                      Circle()
+                        .fill(Color("ColorRed"))
+                      Circle()
+                        .fill(.black.opacity(0.15))
+                        .padding(8)
+                      Image(systemName: "chevron.right.2")
+                        .font(.system(size: 24, weight: .bold))
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnBoardingViewActive = false
+                    }
+                  
+             
+                    
+                    Spacer()
+                  } //: HSTACK
+                } //: FOOTER
+                .frame(height: 80)
+                .padding()
+                
                 Spacer()
                 
             }
