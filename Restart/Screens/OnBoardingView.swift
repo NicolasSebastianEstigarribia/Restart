5a41ff7bf93e5b6a8ce97c6e3323302fc4b33e25
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-21 16:04:49 -0300

    Header section de onboarding view

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -11,16 +11,36 @@ struct OnBoardingView: View {
     @AppStorage("onboarding") var isOnBoardingViewActive: Bool = true
 
     var body: some View {
-        VStack (spacing: 20) {
-            Text("OnBoarding")
-                .font(.title)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all)
             
-            Button {
-                isOnBoardingViewActive = false
-            } label: {
-                Text("Start")
+            
+            // MARK: - HEADER
+            
+            Spacer()
+            VStack {
+                Text("Compartir")
+                    .font(.system(size: 60))
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.white)
+                Text("""
+                No es cuánto damos, pero
+                cuánto amor ponemos en dar.
+                """)
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 10)
             }
-
+            
+            
+            // MARK: - CENTER
+            
+            // MARK: - FOOTER
+            
+            Spacer()
         }
     }
 }
