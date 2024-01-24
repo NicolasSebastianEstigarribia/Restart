ac2e7ab2b23c2f1cc1e11e4c7a93d36c44f5fa8c
Nicolas Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2024-01-24 10:26:13 -0300

    Home view

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -12,15 +12,41 @@ struct HomeView: View {
 
     var body: some View {
         VStack (spacing: 20) {
-            Text("Home View")
-                .font(.title)
+            // MARK: - HEADER
+            Spacer()
+            Image("character-2")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            Spacer()
+            // MARK: - BODY
+            Text("El tiempo que lleva a la maestría depende de la intensidad de nuestro enfoque.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
             
-            Button {
-                isOnBoardingViewActive = true
-            } label: {
-                Text("Restart")
-            }
+            // MARK: - FOOTER
 
+            Button(action: {
+                isOnBoardingViewActive = true
+            }, label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.primary)
+                
+                Text("Reiniciar")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+            })
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            //Diseno del boton
+            
         }
     }
 }
